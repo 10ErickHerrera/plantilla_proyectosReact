20542f84bfaa2d5c31a0678a03059bb75063e889
Erick Herrera <10erickherrera@gmail.com>
2025-04-30 16:45:45 +0000

    Actualizacion plantilla

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-import { BackendURL } from "./component/backendURL";
+
 
 import { Home } from "./pages/home";
 import { Login } from "./pages/login.jsx";
-import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import { Signup } from "./pages/Signup.jsx";
 import { Private } from "./pages/Private.jsx"; // Asegúrate de tener esta página creada
@@ -29,7 +28,6 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
                         <Route element={<Login />} path="/login" />
                         <Route element={<Signup />} path="/signup" />
                         <Route element={<Single />} path="/single/:theid" />
